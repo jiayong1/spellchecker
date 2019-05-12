7526cd6aaae595aaedccbae1ebc1fb6b4f0261d5
Hanyu Wang <hywang66@cs.umd.edu>
2019-05-11 21:33:23 -0400

    deleted old readme

--- cli_checker.py
+++ cli_checker.py
@@ -9,13 +9,12 @@ from checker_backend import SpellChecker
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', '--topk', type=int, default=3, help='return top k canditates to pick')
-    parser.add_argument('-s', '--sigma', type=int, default=5, help='gap score used in global alognment')
+    parser.add_argument('-s', '--sigma', type=int, default=5, help='gap score used in global alignment')
     parser.add_argument('--noBayes', action='store_false', dest='bayes',
                         default=True,
                         help='don\'t apply Bayes rule.')
     parser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
-                        default=False,
-                        help='verbose')
+                        default=False, help='verbose mode on')
     return parser
 
 
@@ -26,7 +25,7 @@ def main():
 
     while True:
         print('Please input your word:')
-        word = str(input('>')).strip().lower()
+        word = str(input('> ')).strip().lower()
         if set(word) - alphabet:
             print('Error: your word contains invalid characters.')
         else:
